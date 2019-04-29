f03c9db976c2040722e368465cb81f17bab8f895
byyft2 <49965911+byyft2@users.noreply.github.com>
2019-04-29 12:43:51 +0900

    Update main.java

--- main.java
+++ main.java
@@ -2,5 +2,6 @@ public class hello{
   public static void main(String[] args){
   System.out.println("반갑습니다");
   System.out.println("First GitHub Training");
+  System.out.println("버그넣지마아아아아아");
   }
  }
